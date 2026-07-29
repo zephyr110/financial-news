af1cccbe2ff64c1d21d373bbf53c5fad0394a3fc
Zephyr <willtien@outlook.com>
2026-07-29 14:21:52 +0800

    fix: remove duplicate SpeedInsights import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "next-themes";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
